3f721120dcebf17525052513f57a834cda882ed2
way-daeho <lavr2755@gmail.com>
2025-01-04 23:41:03 +0900

    Start Python study

---
+++ Lang/1st_Intro/intro.py
@@ -0,0 +1,22 @@
+# for countdown in 5, 4, 3, 2, 1, "hey!":
+# 	print(countdown)
+
+# spell.py
+# spells=[
+# 	"Riddikulus!",
+# 	"Wingardium Leviosa!",
+# 	"Avada Kedavra!",
+# 	"Expecto Patronum!",
+# 	"Nox!",
+# 	"Lumos",
+# ]
+# print(spells[3])
+
+# quotes.py
+quotes={
+	"Moe" : "A wise guy, huh?",
+	"Larry" : "Ow!",
+	"Curly" : "Nyuk nyuk!",
+}
+stooge = "Curly"
+print(stooge, "says:", quotes[stooge])
